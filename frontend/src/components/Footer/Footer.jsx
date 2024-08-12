e8e45d73443ef6e329461d3a10bf31870a1a8c5e
Wycliffe Omondi <odhiambow2354@gmail.com>
2024-08-12 10:32:36 +0300

    Making Homepage responsive in small screens

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.food_dev} alt="logo" className="logo" />
+          <p>
+            The best online food delivery platform, tested and proved to be the
+            most reliable of all time. Just one click away.{" "}
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Products Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>CONTACT US</h2>
+          <ul>
+            <li>Address: 123, XYZ Street, ABC City, 12345</li>
+            <li>Email:dairyproductseshop@gmail.com</li>
+            <li>Phone: 1234567890</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        &copy; 2024 FoodDelv E-Shop. All Rights Reserved
+      </p>
+    </div>
+  );
+};
+export default Footer;
